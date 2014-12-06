d12554756a3ad8282e740f0ab44009e23b6b5990
Jonas <jonas.toth@gmail.com>
2014-12-06 19:58:55 +0100

    Objectbehaviour implemented in basic ways

--- object.cpp
+++ object.cpp
@@ -1,5 +1,20 @@
 #include "object.h"
 
-Object::Object()
+Physix::Object::Object()
 {
+
+}
+
+void Physix::Object::advance(double dt)
+{
+	// new velocity
+	__velocity+= __acceleration * dt;
+
+	// new position
+	__position+= __velocity * dt;
+
+	// clear acceleration, because forces(and therefore acceleration) are calculated in each step
+	// new
+	__acceleration.reset();
+
 }
--- object.h
+++ object.h
@@ -1,10 +1,30 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-class Object
+#include "vector3d.h"
+
+
+namespace Physix
 {
-public:
-	Object();
-};
+
+	class Object
+	{
+	public:
+		Object();
+
+		/**
+		 * @brief advance will calculate the next position/velocity with forward euler
+		 * @param dt how much time passed since last call
+		 */
+		void advance(double dt);
+
+
+	private:
+		Vector3D __position;
+		Vector3D __velocity;
+		Vector3D __acceleration;
+	};
+
+}
 
 #endif // OBJECT_H
