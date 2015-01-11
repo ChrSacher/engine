a51f42639a14f8cc0e3ec1dc43b57d424a97d3f0
Jonas <jonas.toth@gmail.com>
2015-01-11 18:03:47 +0100

    Basemethods implemented.

--- Math/Matrix4D.cpp
+++ Math/Matrix4D.cpp
@@ -3,5 +3,44 @@
 namespace Math
 {
 
+	Math::Matrix4D::Matrix4D()
+	{
+		for(auto i = 0; i < 4; i++)
+			for(auto j = 0; j < 4; j++)
+				data[i][j] = 0;
+	}
+
+	void Math::Matrix4D::setRow(int nbr, Math::Vector4D& row)
+	{
+
+	}
+
+	void Math::Matrix4D::setColumn(int nbr, Math::Vector4D& col)
+	{
+
+	}
+
+	void Math::Matrix4D::loadIdendity()
+	{
+		for(auto i = 0; i < 4; i++)
+			data[i][i] = 1;
+	}
+
+	Math::Vector4D Math::Matrix4D::getRow(int nbr)
+	{
+
+	}
+
+	Math::Vector4D Math::Matrix4D::getColumn(int nbr)
+	{
+
+	}
+
+	Math::Matrix4D Math::idendity()
+	{
+		Matrix4D m;
+		m.loadIdendity();
+		return m;
+	}
 
 }
--- Math/Matrix4D.h
+++ Math/Matrix4D.h
@@ -9,16 +9,57 @@ namespace Math
 	/**
 	 * @brief The Matrix4D class
 	 *
-	 * Computation for Matrices on Processor-side with a numerical type Scalar
-	 * Use it best with float!
+	 * Computation for Matrices on Processor-side with float precision (Opengl uses float)
+	 *
+	 * Row and Column count starts at 0!!!, like in Computer Science, not like Match
 	 */
-	template<typename Scalar>
 	class Matrix4D
 	{
 	public:
+		/**
+		 * @brief Matrix4D initializes the matrix to all zero
+		 */
 		Matrix4D();
 
+		/**
+		 * @brief loadIdendity sets the matrix to the idendity matrix
+		 *
+		 * \sa idendity
+		 */
+		void loadIdendity();
+
+		/**
+		 * @brief setRow
+		 * @param nbr defines which row should be set (0-3)
+		 * @param row defines the values for the row
+		 */
+		void setRow(int nbr, Vector4D& row);
+
+		/**
+		 * @brief setColumn
+		 * @param nbr defines which column should be set (0-3)
+		 * @param col defines the values for the column
+		 */
+		void setColumn(int nbr, Vector4D& col);
+
+		/**
+		 * @brief getRow
+		 * @param nbr which row is used. (0-3)
+		 * @return the wanted row
+		 */
+		Vector4D getRow(int nbr);
+
+		/**
+		 * @brief getColumn
+		 * @param nbr which col is used (0-3)
+		 * @return the wanted column
+		 */
+		Vector4D getColumn(int nbr);
+
 	private:
-		Scalar data[4][4];
+		float data[4][4];
 	};
+
+
+	Matrix4D idendity();
 }
