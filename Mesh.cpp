ef46dc706d9b4d40c85b994d2ea79d365986d318
Christian Sacher <Christian.Sacher@diesterweg-gymnasium.de>
2015-02-20 13:41:15 +0100

    Reworked Entire Structure

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,69 +1,70 @@
 #include "Mesh.h"
 
 
-Mesh::Mesh(void)
-{
-	glGenBuffers(1,&vbo);
-	glGenBuffers(1,&ibo);
-	size=0;
-}
-
-Mesh::Mesh(std::string path)
-{
-	glGenBuffers(1,&vbo);
-	glGenBuffers(1,&ibo);
-	size=0;
-	loadOBJ(path);
-}
-
-
 Mesh::~Mesh(void)
 {
-
-}
-void Mesh::addVertex(Vertex newvertex)
-{
-	size += VERTEXSIZE;
-	glBindBuffer(GL_ARRAY_BUFFER,vbo);
-	glBufferData(GL_ARRAY_BUFFER,sizeof(newvertex),&newvertex,GL_STATIC_DRAW);
-}
-
-void Mesh::addVertices(std::vector<Vertex> newvertices,std::vector<GLuint> indices)
-{
-	size += indices.size();
-	glBindBuffer(GL_ARRAY_BUFFER,vbo);
-	glBufferData(GL_ARRAY_BUFFER,newvertices.size() * sizeof(Vertex),&newvertices[0],GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint),&indices[0],GL_STATIC_DRAW);
+	glDeleteVertexArrays(1,&vao);
 }
 
 void Mesh::draw()
 {
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER,vbo);
-	glVertexAttribPointer(0,3,GL_FLOAT,false,VERTEXSIZE * 4,0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo);
-	glDrawElements(GL_TRIANGLES,size,GL_UNSIGNED_INT,0);
-	glDisableVertexAttribArray(0);
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES,0,Vertices.size());
+	glBindVertexArray(0);
 }
 
-bool Mesh::loadOBJ(std::string path)
+Mesh::Mesh(std::vector<Vertex> vertices)
+{
+	
+	glGenVertexArrays(1,&vao);
+	glBindVertexArray(vao);
+	Vertices=vertices;
+	loadBuffer();
+}
+Mesh::Mesh(std::string path)
+{
+	
+	glGenVertexArrays(1,&vao);
+	glBindVertexArray(vao);
+	loadOBJ(path);
+}
+
+Mesh::Mesh()
+{
+	
+	glGenVertexArrays(1,&vao);
+	glBindVertexArray(vao);
+	
+}
+
+Vertex::Vertex()
+{
+	pos=vec3(0,0,0);
+}
+Vertex::Vertex(const vec3& position,const vec2& tcoord)
+{
+	pos=position;
+	uv=tcoord;
+}
+Vertex::Vertex(float x, float y, float z)
+{
+	pos=vec3(x,y,z);
+}
+
+void Mesh::loadOBJ(std::string path)
 {
 	printf("Loading OBJ file %s...\n", path.c_str());
 
-	std::vector<Vector3> temp_vertices; 
-	std::vector<Vector2> temp_uvs;
-	std::vector<Vector3> temp_normals;
-	GLuint currentvert,currentuv,currentnormal;
-	currentvert = vertices.size();
-	currentuv = uvs.size();
-	currentnormal = normals.size();
+	std::vector<vec3> temp_vertices; 
+	std::vector<vec2> temp_uvs;
+	std::vector<vec3> temp_normals;
+	std::vector<GLuint> vertexIndices,uvIndices,normalIndices;
 
 
 	FILE * file = fopen(path.c_str(), "r");
 	if( file == NULL ){
 		printf("Impossible to open the file %s ",path.c_str());
-		return false;
+		return ;
 	}
 
 	while( 1 )
@@ -79,20 +80,20 @@ bool Mesh::loadOBJ(std::string path)
 		
 		if ( strcmp( lineHeader, "v" ) == 0 )
 		{
-			Vector3 vertex;
+			vec3 vertex;
 			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
 			temp_vertices.push_back(vertex);
 		}
 		else if ( strcmp( lineHeader, "vt" ) == 0 )
 		{
-			Vector2 uv;
+			vec2 uv;
 			fscanf(file, "%f %f\n", &uv.x, &uv.y );
 			uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
 			temp_uvs.push_back(uv);
 		}
 		else if ( strcmp( lineHeader, "vn" ) == 0 )
 		{
-			Vector3 normal;
+			vec3 normal;
 			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
 			temp_normals.push_back(normal);
 		}
@@ -104,17 +105,17 @@ bool Mesh::loadOBJ(std::string path)
 			if (matches != 9)
 			{
 				printf("File %s cannot be read",path.c_str());
-				return false;
+				return ;
 			}
-			vertexIndices.push_back(vertexIndex[0] - 1 + currentvert);
-			vertexIndices.push_back(vertexIndex[1] - 1 + currentvert);
-			vertexIndices.push_back(vertexIndex[2] - 1 + currentvert);
-			uvIndices    .push_back(uvIndex[0] - 1 + currentuv);
-			uvIndices    .push_back(uvIndex[1] - 1 + currentuv);
-			uvIndices    .push_back(uvIndex[2] - 1 + currentuv);
-			normalIndices.push_back(normalIndex[0] - 1 + currentnormal);
-			normalIndices.push_back(normalIndex[1] - 1 + currentnormal);
-			normalIndices.push_back(normalIndex[2] - 1 + currentnormal);
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+			uvIndices    .push_back(uvIndex[0]);
+			uvIndices    .push_back(uvIndex[1] );
+			uvIndices    .push_back(uvIndex[2]);
+			normalIndices.push_back(normalIndex[0]);
+			normalIndices.push_back(normalIndex[1]);
+			normalIndices.push_back(normalIndex[2]);
 		}else
 		{
 			// Probably a comment, eat up the rest of the line
@@ -123,40 +124,48 @@ bool Mesh::loadOBJ(std::string path)
 		}
 
 	}
-
-	// For each vertex of each triangle
-	for( GLubyte i=0; i<temp_vertices.size(); i++ )
+	for( unsigned int i=0; i<vertexIndices.size(); i++ )
 	{
-		vertices.push_back( temp_vertices[ i ]);
+		glm::vec3 vertex = temp_vertices[ vertexIndices[i] - 1 ];
+		glm::vec2 uv = temp_uvs[ uvIndices[i] - 1 ];
+		Vertices.push_back(Vertex(vertex,uv));
 	}
-	for( GLubyte i=0; i<temp_uvs.size(); i++ )
-	{
-		uvs     .push_back(temp_uvs[i]);
-	}
-	for( GLubyte i=0; i<temp_normals.size(); i++ )
-	{
-		normals .push_back(temp_normals[i]);
-	}
-	for(int i=0;i<temp_vertices.size();i++)
-	{
-		Vertices.push_back(Vertex(temp_vertices[i]));
-	}
-	return true;
-
-}
+	loadBuffer();
+	
+};
 
 void Mesh::loadBuffer()
 {
-	addVertices(Vertices,vertexIndices);
+	
+	std::vector<vec3> positions;
+	std::vector<vec2> uvs;
+	positions.reserve(Vertices.size());
+	uvs.reserve(Vertices.size());
+	for(int i = 0;i< Vertices.size();i++)
+	{
+		positions.push_back(*Vertices[i].getPos());
+		uvs.push_back(*Vertices[i].getUV());
+	}
+	glGenBuffers(NUMBUFFERS,vab);
+
+	glBindBuffer(GL_ARRAY_BUFFER,vab[POSITIONVB]);
+	glBufferData(GL_ARRAY_BUFFER,Vertices.size() * sizeof(positions[0]),&positions[0],GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0);
+
+	
+
+	glBindBuffer(GL_ARRAY_BUFFER,vab[TEXTUREVB]);
+	glBufferData(GL_ARRAY_BUFFER,Vertices.size() * sizeof(uvs[0]),&uvs[0],GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,0,0);
+
+	glBindVertexArray(0);
 }
- 
+
 void Mesh::clearData()
 {
-	vertices.clear();
-	uvs.clear();
-    normals.clear();
 	Vertices.clear();
-	vertexIndices.clear();
-	uvIndices.clear();
-	normalIndices.clear();
+	Vertices.push_back(Vertex(vec3(0,0,0),vec2(0,0)));
+	loadBuffer();
 }
--- Mesh.h
+++ Mesh.h
@@ -1,36 +1,46 @@
 #pragma once
-
+#include <glm/glm.hpp>
 #include <glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <GL/GLu.h>
-#include "Vertex.h"
 #include <vector>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include "Math\vectors.h"
-
-class Mesh
+using namespace glm;
+class Vertex
 {
-
 public:
-	Mesh(void);
-	Mesh(std::string path);
-	~Mesh(void);
-
-	GLuint vbo,size,ibo;
-	void addVertex(Vertex newvertex);
-	void addVertices(std::vector<Vertex> newvertices,std::vector<GLuint> indices);
-	bool loadOBJ(std::string path);
-	void loadBuffer();
-    void clearData();
-	void draw();
-	std::vector<Vector3> vertices;
-    std::vector<Vector2> uvs;
-    std::vector<Vector3> normals;
-	std::vector<Vertex> Vertices;
-	std::vector<GLuint> vertexIndices,uvIndices,normalIndices;
+	Vertex();
+	Vertex(const vec3& position,const vec2& tcoord);
+	Vertex(float x,float y,float z);
+	inline vec3* getPos(){return &pos;};
+	inline vec2* getUV(){return &uv;};
+	vec3 pos;
+	vec2 uv;
 
 };
 
+
+class Mesh
+{
+public:
+	Mesh(void);
+	virtual ~Mesh(void);
+	Mesh(std::vector<Vertex> vertices);
+	Mesh(std::string path);
+	std::vector<Vertex> Vertices;
+	void draw();
+	void loadOBJ(std::string path);
+	void loadBuffer();
+	void clearData();
+private:
+	Mesh(const Mesh& other);
+	void operator=(const Mesh& other);
+	enum
+	{
+		POSITIONVB,
+		TEXTUREVB,
+		NUMBUFFERS
+	};
+	GLuint vao,vbo;
+	GLuint vab[NUMBUFFERS];
+};
